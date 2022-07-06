fb813e60c4925bb7113411aa27ef35b751487519
Neha Tomar <tneha5161@gmail.com>
2022-07-06 20:09:40 +0530

    creating a chatapp

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { initializeApp } from "firebase/app";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const firebaseConfig = {
+  apiKey: "AIzaSyBg2bl06Lk7nnhHpG4Oz8HOyJBxjIWsGsY",
+  authDomain: "react-chat-app-75abd.firebaseapp.com",
+  databaseURL: "https://react-chat-app-75abd-default-rtdb.firebaseio.com",
+  projectId: "react-chat-app-75abd",
+  storageBucket: "react-chat-app-75abd.appspot.com",
+  messagingSenderId: "1038641374819",
+  appId: "1:1038641374819:web:9ba09ecb4ac1167f367a56",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
+// root.render(
+
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
