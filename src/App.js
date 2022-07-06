7a3f80df2f9b2d6160275a016c8fdf70bb0749cb
Neha Tomar <tneha5161@gmail.com>
2022-07-06 22:22:40 +0530

    add google auth

--- src/App.js
+++ src/App.js
@@ -1,50 +1,84 @@
 import { useState, useEffect } from "react";
 import { getDatabase, ref, push, set, onChildAdded } from "firebase/database";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 
 import "./App.css";
 
 function App() {
-  const [name, setName] = useState("");
+  const [user, setUser] = useState("");
   const [chats, setChats] = useState([]);
   const [msg, setMsg] = useState("");
 
   const db = getDatabase();
   const chatListRef = ref(db, "chats");
 
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+
+  const googleLogin = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        const user = result.user;
+        setUser({ name: result.user.displayName, email: result.user.email });
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.customData.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
+
+  const updateHeight = () => {
+    const el = document.getElementById("chat");
+    if (el) {
+      el.scrollTop = el.scrollHeight;
+    }
+  };
+
   useEffect(() => {
     onChildAdded(chatListRef, (data) => {
       setChats((chats) => [...chats, data.val()]);
+      setTimeout(() => {
+        updateHeight();
+      }, 100);
     });
   }, []);
 
   const sendChat = () => {
     const chatRef = push(chatListRef);
-    set(chatRef, { name, message: msg });
-
-    // const c = [...chats];
-    // c.push();
-    // setChats(c);
+    set(chatRef, { user, message: msg });
     setMsg("");
   };
   return (
     <div className="main">
-      {name ? null : (
+      {user.email ? null : (
         <div className="top">
-          <input
-            type="text"
-            placeholder="Enter name to start"
-            onBlur={(e) => setName(e.target.value)}
-          />
+          <button
+            onClick={(e) => {
+              googleLogin();
+            }}
+          >
+            Google SignIn
+          </button>
         </div>
       )}
-      {name ? (
+      {user.email ? (
         <div className="bottom">
-          <h3>User:{name}</h3>
-          <div className="chat-container">
+          <h3>User:{user.name}</h3>
+          <div id="chat" className="chat-container">
             {chats.map((c) => (
-              <div className={`container ${c.name === name ? "me" : ""}`}>
+              <div
+                className={`container ${
+                  c.user.email === user.email ? "me" : ""
+                }`}
+              >
                 <p className="chatbox">
-                  <strong>{c.name}:</strong>
+                  <strong>{c.user.name}:</strong>
                   <span>{c.message}</span>
                 </p>
               </div>
